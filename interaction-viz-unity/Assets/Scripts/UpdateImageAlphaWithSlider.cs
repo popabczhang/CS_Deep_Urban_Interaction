2fe042712c8f937bc2d4c1ed017548ba68e3eeef
Ryan Yan Zhang <ryanz@mit.edu>
2018-03-18 00:22:58 -0400

    Add Video Preview in UI and Slider for Alpha control

---
+++ interaction-viz-unity/Assets/Scripts/UpdateImageAlphaWithSlider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateImageAlphaWithSlider : MonoBehaviour {
+
+	public Slider slider;
+
+	void OnSlider()
+    {
+        Image img = GetComponent<Image>();
+        Color c = img.material.color;
+        img.material.color = new Color(c.r, c.g, c.b, slider.value);
+        Debug.Log("now!");
+    }
+
+}
